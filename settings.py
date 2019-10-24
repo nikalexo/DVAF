96fd3f3d87e5e2c5a7c0023128a99c5b8eddba27
Nikolaos Alexopoulos <alexopoulos@tk.tu-darmstadt.de>
2019-10-24 13:48:07 +0200

    changes

---
+++ settings.py
@@ -0,0 +1,6 @@
+SPLASH_URL = 'http://localhost:8050'
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
